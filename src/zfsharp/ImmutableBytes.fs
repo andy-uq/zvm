6439156a41c1a87033c909dca7c664c4fe0fbd2d
Andy Clarke <andy.uq@gmail.com>
2016-05-28 19:34:33 +1200

    Immutable byte store

---
+++ src/zfsharp/ImmutableBytes.fs
@@ -0,0 +1,30 @@
+﻿module Immutable_bytes
+  open Types
+  open Utility
+
+  type t =  { original_bytes : string; edits : IntMap<char> }
+
+  let make bytes =
+    { original_bytes = bytes; edits = Map.empty }
+
+  let size bytes =
+    String.length bytes.original_bytes
+
+  let read_byte bytes address =
+    if is_out_of_range address (size bytes) then
+      failwith "address is out of range"
+    else
+      let (Byte_address addr) = address in
+      let c =
+        match (Map.tryFind addr bytes.edits) with
+        | None -> bytes.original_bytes.[addr]
+        | Some a -> a in
+      int c
+
+  let write_byte bytes address value =
+    if is_out_of_range address (size bytes) then
+      failwith "address is out of range"
+    else
+      let (Byte_address addr) = address in
+      let b = char value in
+      { bytes with edits = Map.add addr b bytes.edits }
