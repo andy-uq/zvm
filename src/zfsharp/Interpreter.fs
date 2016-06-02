caba621ef976109c5d9e053059466fdaa0cf65a4
Andy Clarke <andy.clarke@ubiquity.co.nz>
2016-06-02 14:02:51 +1200

    Implementing jump and store/load

--- src/zfsharp/Interpreter.fs
+++ src/zfsharp/Interpreter.fs
@@ -1,5 +1,6 @@
 ﻿module Interpreter
   open Types
+  open Utility
 
   type t =
     {
@@ -103,7 +104,36 @@
     | Some (sense, Branch_address branch_target) ->
       if result = sense then set_program_counter interpreter branch_target
       else set_program_counter interpreter following
+  
+  let interpret_instruction interpreter instruction handler = 
+    let (result, handler_interpreter) = handler interpreter
+    let store = Instruction.store instruction
+    let store_interpreter = interpret_store handler_interpreter store result
+    interpret_branch store_interpreter instruction result
 
+  let interpret_value_instruction interpreter instruction handler =
+    let result = handler interpreter
+    let store = Instruction.store instruction
+    let store_interpreter = interpret_store interpreter store result
+    interpret_branch store_interpreter instruction result
+
+  let interpret_effect_instruction interpreter instruction handler =
+    let handler_interpreter = handler interpreter
+    let result = 0
+    let store = Instruction.store instruction
+    let store_interpreter = interpret_store handler_interpreter store result
+    interpret_branch store_interpreter instruction result
+
+  (* This routine handles all call instructions:
+     2OP:25  call_2s  routine arg -> (result)
+     2OP:26  call_2n  routine arg
+     1OP:136 call_1s  routine -> (result)
+     1OP:143 call_1n  routine
+     VAR:224 call_vs  routine up-to-3-arguments -> (result)
+     VAR:236 call_vs2 routine up-to-7-arguments -> (result)
+     VAR:249 call_vn  routine up-to-3-arguments
+     VAR:250 call_vn2 routine up-to-7-arguments
+     The "s" versions store the result; the "n" versions discard it. *)
   let handle_call routine_address arguments interpreter instruction =
     if routine_address = 0 then
       let result = 0
@@ -120,17 +150,121 @@
       let pc = Routine.first_instruction interpreter.story routine_address
       set_program_counter (add_frame interpreter frame) pc
 
+
+  (* Spec: 2OP:1 je a b ?(label)
+     Jump if a = b. *)
+  let handle_je2 a b interpreter =
+    if a = b then 1 else 0
+
+  let handle_je3 a b c interpreter =
+    if a = b || a = c then 1 else 0
+
+  let handle_je4 a b c d interpreter =
+    if a = b || a = c || a = d then 1 else 0
+
+  (* Spec: 2OP:2 jl a b ?(label)
+     Jump if a < b  using a signed 16-bit comparison. *)
+  let handle_jl a b interpreter =
+    let a = signed_word a
+    let b = signed_word b
+    if a < b then 1 else 0
+
+  (* Spec: 2OP:3 jg a b ?(label)
+     Jump if a > b  using a signed 16-bit comparison. *)
+  let handle_jg a b interpreter =
+    let a = signed_word a
+    let b = signed_word b
+    if a > b then 1 else 0
+
+  (* Spec: 2OP:20 add a b -> (result)
+     Signed 16-bit addition. *)
+  let handle_add a b interpreter =
+    a + b
+
+  (* Spec: 2OP:21 add a b -> (result)
+     Signed 16-bit subtraction. *)
+  let handle_sub a b interpreter =
+    a - b
+
+  (* Spec: 2OP:22 add a b -> (result)
+     Signed 16-bit multiplication. *)
+  let handle_mul a b interpreter =
+    a * b
+
+  (* Spec: 2OP:23 add a b -> (result)
+     Signed 16-bit division. *)
+  let handle_div a b interpreter =
+    let a = signed_word a
+    let b = signed_word b
+    a / b
+
+  (* Spec: 2OP:24 add a b -> (result)
+     Signed 16-bit modulo. *)
+  let handle_mod a b interpreter =
+    let a = signed_word a
+    let b = signed_word b
+    a % b
+
+  (* Spec: 1OP:128 jz a ?(label)
+     Jump if a = 0. *)
+  let handle_jz a interpreter =
+    if a = 0 then 1 else 0
+
+  let handle_loadw arr idx interpreter =
+    let arr = Word_address arr
+    let addr = inc_word_addr_by arr idx
+    Story.read_word interpreter.story addr
+
+  let handle_loadb arr idx interpreter =
+    let arr = Byte_address arr
+    let addr = inc_byte_addr_by arr idx
+    Story.read_byte interpreter.story addr
+
+  let handle_storew arr idx value interpreter =
+    let arr = Word_address arr
+    let addr = inc_word_addr_by arr idx
+    { interpreter with story = Story.write_word interpreter.story addr value }
+
+  let handle_storeb arr idx value interpreter =
+    let arr = Byte_address arr
+    let addr = inc_byte_addr_by arr idx
+    { interpreter with story = Story.write_byte interpreter.story addr value }
+
   let handle_ret result interpreter =
-      interpret_return interpreter result
+    interpret_return interpreter result
+
+  let handle_jump offset interpreter instruction =
+    let offset = signed_word offset
+    let target = Instruction.jump_address instruction offset
+    set_program_counter interpreter target
 
   let step_instruction interpreter =
-    let instruction = Instruction.decode interpreter.story interpreter.program_counter in
-    let operands = Instruction.operands instruction in
-    let (arguments, interpreter) = operands_to_arguments interpreter operands in
-    let opcode = Instruction.opcode instruction in
+    let instruction = Instruction.decode interpreter.story interpreter.program_counter
+    let operands = Instruction.operands instruction
+    let (arguments, interpreter) = operands_to_arguments interpreter operands
+    let interpret_instruction = interpret_instruction interpreter instruction
+    let value = interpret_value_instruction interpreter instruction
+    let effect = interpret_effect_instruction interpreter instruction
+    let opcode = Instruction.opcode instruction
     match (opcode, arguments) with
+    | (OP2_1, [a; b]) -> value (handle_je2 a b)
+    | (OP2_1, [a; b; c]) -> value (handle_je3 a b c)
+    | (OP2_1, [a; b; c; d]) -> value (handle_je4 a b c d)
+    | (OP2_2, [a; b]) -> value (handle_jl a b)
+    | (OP2_3, [a; b]) -> value (handle_jg a b)
+    | (OP2_15, [arr; idx]) -> value (handle_loadw arr idx)
+    | (OP2_16, [arr; idx]) -> value (handle_loadb arr idx)
+    | (OP2_20, [a; b]) -> value (handle_add a b)
+    | (OP2_21, [a; b]) -> value (handle_sub a b)
+    | (OP2_22, [a; b]) -> value (handle_mul a b)
+    | (OP2_23, [a; b]) -> value (handle_div a b)
+    | (OP2_24, [a; b]) -> value (handle_mod a b)
+    | (OP1_128, [a]) -> value (handle_jz a)
     | (OP1_139, [result]) -> handle_ret result interpreter
+    | (OP1_140, [offset]) -> handle_jump offset interpreter instruction
     | (VAR_224, routine :: args) -> handle_call routine args interpreter instruction
+    | (VAR_225, [arr; ind; value]) -> effect (handle_storew arr ind value)
+    | (VAR_226, [arr; ind; value]) -> effect (handle_storeb arr ind value)
     | _ -> failwith (Printf.sprintf "TODO: %s " (Instruction.display instruction interpreter.story))
 
   let display_current_instruction interpreter =
@@ -141,4 +275,4 @@
   let display interpreter =
     let frames = Frameset.display interpreter.frames
     let instr = display_current_instruction interpreter
-    Printf.sprintf "\n---\n%s\n%s\n" frames instr
+    Printf.sprintf "\n---\n%s\n%s" frames instr
--- src/zfsharp/Program.fs
+++ src/zfsharp/Program.fs
@@ -6,12 +6,10 @@ open Utility
 
 [<EntryPoint>]
 let main argv = 
+  let rec interpreter_loop interpreter = 
+    System.Console.Write (Interpreter.display_current_instruction interpreter);
+    interpreter_loop (Interpreter.step_instruction interpreter)
+  
   let story = Story.load "minizork.z3"
-  let interpreter1 = Interpreter.make story
-  let interpreter2 = Interpreter.step_instruction interpreter1
-  let interpreter3 = Interpreter.step_instruction interpreter2
-  let text1 = Interpreter.display interpreter1
-  let text2 = Interpreter.display interpreter2
-  let text3 = Interpreter.display interpreter3
-  Printf.printf "%s\n%s\n%s\n" text1 text2 text3  
+  interpreter_loop (Interpreter.make story)
   0 // return an integer exit code
